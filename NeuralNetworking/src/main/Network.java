ea8e269e55685e2f124a029d70df8e57fa7ed8c2
Henry Loh <pgloh.henry@gmail.com>
2016-12-10 20:30:12 -0800

    Added Graphics

--- NeuralNetworking/src/main/Network.java
+++ NeuralNetworking/src/main/Network.java
@@ -8,9 +8,15 @@ public class Network {
 	static float[] outputs = new float[12];
 	static String[] outputNames = {"Random","English","Chinese","German","Italian","Spanish","Latin","Dutch","French","Portuguese","Gailic","Icelandic"};
 	
+	static int timesToTrain = 100;
+	static String testWord = "NA";
 	static float outputSum = 1;
+	static int prediction = 0;
 	static float step = 0.001f;
 
+	static boolean[] successes = new boolean[200];
+	static int successesIndex = 0;
+	
 	public static void refresh (){
 		for (int i=0;i<inputs.length;i++) inputs[i] = 0;
 		for (int i=0;i<mid.length;i++) mid[i] = 0;
@@ -53,6 +59,22 @@ public class Network {
 		outputSum = 1/sum;
 	}//normalize method
 				
+	public static int maxIndex(float [] outputs){
+		int max = 0;
+		for (int i=0;i<outputs.length;i++){
+			if (outputs[i] > outputs[max]){
+				max = i;
+			}
+		}
+		return max;
+	}//maxIndex method
+	
+	public static String successRate(){
+		int correct = 0;
+		for (int i=0;i<successes.length;i++) if (successes[i]) correct++;
+		return String.format("%.2f",(float)correct/successes.length);
+	}//successRate method
+	
 	public static void test(String s, boolean verb){
 
 		if (verb) System.out.println("Testing string "+s);
@@ -63,11 +85,15 @@ public class Network {
 		}
 		refresh();
 		
+		testWord = s;
+		
 		loadInputs(s);
 		travelSynapse(1);
 		travelSynapse(2);
 		
 		normalize();
+		prediction = maxIndex(outputs);
+		
 		if (verb) for (int i=0;i<12;i++) System.out.printf("%11s -- %.2f%%\n",outputNames[i],100*outputs[i]);
 
 	}//test input
@@ -144,7 +170,11 @@ public class Network {
 	}//trainSynapse2 method
 	
 	public static void train(String s, int ans){
-		
+		//Load input into system
+		test(s,false);
+		if (successesIndex == successes.length) successesIndex = 0;
+		successes[successesIndex] = prediction == ans;
+		successesIndex++;
 		//Synapse 1
 		for (int i=0;i<synapses[0].length;i++){
 			//test(s,false);
@@ -156,11 +186,11 @@ public class Network {
 			boolean didInc = false;
 
 			//Check train increase
-			if (synapses[0][i] < 1)
+			if (synapses[0][i] <= 1-step)
 				didInc = trainSynapse1(i,ans,step);
 			
 //			//Check train decrease
-			if (!didInc && synapses[0][i] > 0)
+			if (!didInc && synapses[0][i] >= step)
 				trainSynapse1(i,ans,-step);
 
 		}//1
@@ -183,10 +213,7 @@ public class Network {
 		
 	}//train method
 	
-	public static void main(String[] args){
-		//Start Time Marker
-		long start = System.currentTimeMillis();
-
+	public static void init(){
 		//Declare Synapses
 		synapses[0] = new float[16*26*16];
 		synapses[1] = new float[16*12];
@@ -194,7 +221,15 @@ public class Network {
 		//Fill Synapses With Random Values
 		for (int i=0;i<synapses[0].length;i++) synapses[0][i] = (float) Math.random();
 		for (int i=0;i<synapses[1].length;i++) synapses[1][i] = (float) Math.random();
+		
+	}//init method
+	
+	public static void main(String[] args){
+		//Start Time Marker
+		long start = System.currentTimeMillis();
 
+		init();
+		
 		//Create a Copy of Initial Synapse Values
 		float[][] first_synapses = {synapses[0].clone(),synapses[1].clone()};
 
@@ -209,11 +244,12 @@ public class Network {
 		int len = english.count;
 		if (dutch.count < len) len = dutch.count;
 		
+		System.out.println("Training Count: "+timesToTrain);
 		System.out.printf("English: %d words.\nDutch: %d words.\n",english.count,dutch.count);
 		//Time Marker
 		float trainingStartTime = System.currentTimeMillis();
 
-		for (int trainingCounts=0;trainingCounts<1000;trainingCounts++){
+		for (int trainingCounts=0;trainingCounts<timesToTrain;trainingCounts++){
 			System.out.println("Training Round "+(trainingCounts+1));
 			for (int i=0;i<len;i++){
 				train(english.getWord(i),1);
@@ -232,12 +268,19 @@ public class Network {
 		//End Time Marker
 		long end = System.currentTimeMillis();
 		System.out.println("Time Elapsed: "+(end-start));
-
+		
 		processArrayChanges("Synapse1",first_synapses[0],synapses[0]);
 		processArrayChanges("Synapse2",first_synapses[1],synapses[1]);
 		
 	}//main method
 
+	public static void reset(){
+		refresh();
+		init();
+		successes = new boolean[200];
+		
+	}//reset method
+	
 	public static void processArrayChanges(String name, float[] src, float[] dest){
 		if (src.length != dest.length){
 			System.out.println("Compare Arrays do not have the same length!!!");
